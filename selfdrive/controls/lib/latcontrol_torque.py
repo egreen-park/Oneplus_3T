b4b8edfb67941074844bfe06fad938c3f4bd260e
egreen-park <egreen_park@naver.com>
2022-11-25 22:52:01 +0900

    update

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -21,8 +21,7 @@ from selfdrive.ntune import ntune_torque_get
 # move it at all, this is compensated for too.
 
 LOW_SPEED_X = [0, 10, 20, 30]
-LOW_SPEED_Y = [15, 13, 10, 5] #comma
-#LOW_SPEED_Y = [15, 2, 2, 2] 
+LOW_SPEED_Y = [13, 10, 5, 1]
 
 class LatControlTorque(LatControl):
   def __init__(self, CP, CI):
@@ -71,10 +70,11 @@ class LatControlTorque(LatControl):
 
       if isLowSpeed:
         #low_speed_factor = interp(CS.vEgo, [0, 15], [500, 0]) # comma 1st
-        low_speed_factor = interp(CS.vEgo, [0, 10, 20], [500, 500, 200]) # comma 2nd
-        #low_speed_factor = interp(CS.vEgo, LOW_SPEED_X, LOW_SPEED_Y)**2
+        #low_speed_factor = interp(CS.vEgo, [0, 10, 20], [500, 500, 200]) # comma 2nd
+        #low_speed_factor = interp(CS.vEgo, LOW_SPEED_X, LOW_SPEED_Y)**2 #
+        low_speed_factor = interp(CS.vEgo, [0, 5, 10, 15], [300, 200, 80, 0]) # PBI Test
       else:
-        low_speed_factor = interp(CS.vEgo, [0, 5], [300, 0])
+        low_speed_factor = interp(CS.vEgo, [0, 5, 7], [300, 200, 0])
 
       setpoint = desired_lateral_accel + low_speed_factor * desired_curvature
       measurement = actual_lateral_accel + low_speed_factor * actual_curvature
